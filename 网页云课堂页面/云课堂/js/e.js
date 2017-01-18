177056842f3fa06608d9eca655ea81d9089285aa
xswxsw2012 <15111980348@163.com>
2017-01-18 17:52:19 +0800

    做头部cookie，发现需要服务器支持，没做好。。

--- 网页云课堂页面/云课堂/js/e.js
+++ 网页云课堂页面/云课堂/js/e.js
@@ -80,7 +80,89 @@ window.onload=function(){
 	picture1.onmouseover=play;
 	play();
 
-/*************************关闭顶部通知条，cookie实现******************************************/
-	
+/*************************关闭顶部通知条，cookie实现************************本地获取不到，可能要服务器******************/
+	//取节点
+	var touBtn = document.getElementsByClassName("tou1-4")[0];
+	var tou = document.getElementsByClassName("tou")[0];
+	var toucookie,key;
+
+	//跨浏览器事件
+	var EventUtil = {
+		addHandler: function(element, type, handler){
+			if(element.addEventListener){
+				element.addEventListener(type, handler, false);
+			}else if(element.attachEvent){
+				element.attachEvent("on" + type, handler);
+			}else{
+				element["on" + type] = handler;
+			}
+		},
+		removeHandler: function(element, type, handler){
+			if(element.removeEventListener){
+				element.removeEventListener(type, handler, false);
+			}else if(element.detachEvent){
+				element.detachEvent("on" + type, handler);
+			}else{
+				element["on" + type] = null;
+			}
+		}
+	};
+
+	//点击头部关闭按钮执行函数
+	var handler = function(){
+		setCookie("toucookie","123",1000);
+		// document.cookie = "key=123";
+		console.log(getCookie("toucookie"));
+		// checkCookie();
+		tou.style.display = "none";
+
+	};
+
+	EventUtil.addHandler(touBtn, "click", handler);
+
+
+	//创建cookie
+	function setCookie(c_name,value,expiredays)
+	{
+		var exdate=new Date()
+		exdate.setDate(exdate.getDate()+expiredays)
+		document.cookie=c_name+ "=" +escape(value)+
+		((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
+	}
+
+	//得到cookie
+	function getCookie(name)
+	{
+		if (document.cookie.length > 0) 
+		{
+			start = document.cookie.indexOf(name + "=");
+			if(start != -1){
+				start += name.length + 1;
+				end = document.cookie.indexOf(";",start);
+				if (end == -1) 
+				{
+					end = document.cookie.length;
+				}
+				return unescape(document.cookie.substring(start,end))
+			}
+		}
+		return ""
+	}
+
+	//检测cookie
+	function checkCookie()
+	{
+		toucookie = getCookie('toucookie')
+		if (toucookie!=null && toucookie!="") 
+		{
+			alert('Welcome again' +toucookie+'!')
+		}else{
+			toucookie=prompt('Please enter your name:',"")
+			if(toucookie!=null&&toucookie!=""){
+				setCookie('toucookie',123,365)
+			}
+		}
+	}
+
 
 }
