d044cb7d9e9cfa551ae4fe00783c4682da82d35f
xswxsw2012 <15111980348@163.com>
2017-02-06 17:52:00 +0800

    做了轮播图点击功能，主内容区ajax请求数据正在完善

--- 网页云课堂页面/云课堂/js/e.js
+++ 网页云课堂页面/云课堂/js/e.js
@@ -80,6 +80,7 @@ window.onload=function(){
 	picture1.onmouseover=play;
 	play();
 
+
 /*************************关闭顶部通知条，cookie实现************************本地获取不到，可能要服务器******************/
 	//取节点
 	var touBtn = document.getElementsByClassName("tou1-4")[0];
@@ -164,5 +165,66 @@ window.onload=function(){
 		}
 	}
 
+/************************************部分公用函数**********************************************************************/
+
+	//添加类名，如果类名存在，则不添加
+	function addClassName(node,name){
+		var arr = node.className.split('');
+		if(node.className == ''){
+			node.className = name;
+		}else if(arr.indexOf(name) == -1){  //indexOf()返回-1表示要检索的字符串值没有出现
+			node.className += ' ' + name;
+		}
+	}
+
+	//删除类名，为了防止类名相同
+	function removeClassName(node,name){
+		var arr = [];
+		if(node.className != ''){
+			arr = node.className.split(' ');
+			var _index = arr.indexOf(name); //找到name的位置
+			if(_index != -1){
+				arr.splice(_index,1);  //从index处开始删除元素
+				node.className = arr.join(' ');
+			}
+		}
+	}
+
+	//设置参数格式(get方法中常用于向服务器查询某些信息，添加在URL的末尾，将信息发送给服务器)
+	function serialize (data) {
+		if(!data) return '';
+		var pairs = [];
+		for(var name in data){
+			if(!data.hasOwnProperty(name)) continue;
+			if(typeof data[name] == 'function') continue;
+			var value = data[name].toString();
+			name = encodeURIComponent(name);//查询字符串中每个参数的名称和值必须使用encodeURIComponent()进行编码
+			value = encodeURIComponent(value);
+			pairs.push(name + '=' + value);
+		}
+		return pairs.join('&'); //所有名-值对必须由和号(&)分隔
+	}
+
+	//封装get方法
+	function get(url,data,callback){
+		var xhr = new XMLHttpRequest();
+		data = (data == ''&&data == ' ')?' ':data; //不太明白
+		xhr.onreadystatechange = function(){ //在调用open()之前指定onreadystatechange事件处理程序确保跨浏览器兼容性
+			if(xhr.readyState == 4){ //4表示已经接收到全部响应数据
+				if(xhr.status >= 200 && xhr.status < 300||xhr.status == 304){//响应的HTTP状态，304表示请求的资源没有被修改，可以使用浏览器中缓存的版本,响应也是有效的
+					callback(xhr.responseText); //作为响应主体被返回的文本
+				}else{
+					alert('Request was unsuccessfull:' + xhr.status);
+				}
+			}
+		}
+		if (data != ' ') {
+			url += '?' + serialize(data); //在URL后加查询字符串
+		}else {
+			url += '?';
+		}
+		xhr.open('get',url,true); //true表示异步请求
+		xhr.send(null);
+	}
 
 }
